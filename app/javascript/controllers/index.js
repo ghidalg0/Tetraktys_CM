7cbd9e81d6aa01dec8c1ae92203a6e12ad9b2f92
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-06-14 19:57:02 +0200

    ✅  underlining working

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
+import ActiveMenuTabController from "./active_menu_tab_controller.js"
+application.register("active-menu-tab", ActiveMenuTabController)
+
+import HelloController from "./hello_controller.js"
 application.register("hello", HelloController)
